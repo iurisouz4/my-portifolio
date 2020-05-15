a18524487e1ed91925c2b63665f700b98f33c421
Iuri Souza <iuri.santanna@tvglobo.com.br>
2020-05-14 21:50:03 -0300

    layout changes

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'iurisouz4'
-export const siteTitle = 'Next.js Sample Website'
+const name = "iurisouz4";
+export const siteTitle = "iurisouz4";
 
 export default function Layout({ children, home }) {
   return (
@@ -13,14 +13,9 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Hello, i'm Iuri Souza. I'm a software engineer..."
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content="/images/profile.jpg" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
@@ -62,5 +57,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
